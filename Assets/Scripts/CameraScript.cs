5266a3192c8fc4e695068f2ba37cdb269c1a913e
Arthur-Melchior <Kallenne1721@gmail.com>
2025-12-09 00:41:41 +0100

    added second camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -1,27 +1,39 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] private Transform trackingTarget;
     [SerializeField] private Vector3 offset;
     [SerializeField] private float smoothSpeed = 5f;
+
+    [SerializeField] private Camera cam1;
+    [SerializeField] private Camera cam2;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        cam1.enabled = true;
+        cam2.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     private void LateUpdate()
     {
         var desiredPosition = trackingTarget.position + offset;
         transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+    }
+
+    public void ChangeCamera(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("oskour");
+        cam1.enabled = !cam1.enabled;
+        cam2.enabled = !cam2.enabled;
     }
 }
